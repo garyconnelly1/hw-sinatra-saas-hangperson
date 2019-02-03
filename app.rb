6cf9981a250e3e4d500eeefe160f0ec004eaa8e2
garyconnelly1 <G00336837@gmit.ie>
2019-02-03 18:43:29 +0000

    .

--- app.rb
+++ app.rb
@@ -55,6 +55,12 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+     if @game.check_win_or_lose == :win
+        redirect '/win'
+    elsif  @game.check_win_or_lose == :lose
+        redirect '/lose'
+      end
+    
     erb :show # You may change/remove this line
   end
   
